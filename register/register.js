36e58284a275caaaea18f2a72a988971fc6c7d5c
bsessou <bignon-boris-freuxy.sessou@etudiant.univ-rennes1.fr>
2024-07-19 16:08:02 +0200

    Remove error message if success

--- register/register.js
+++ register/register.js
@@ -3,14 +3,15 @@
 //Déclarations username
 const username= document.getElementById('username');
 const usercontent=document.getElementById('usercontent');
-const msg_user= "Username must be at least 3 characters";
+const msg_user_low= "Username must be at least 3 characters";
+const msg_user_long= "Username must be less than 15 characters";
 
 //Déclarations email
 const email=document.getElementById('email');
 const emailcontent=document.getElementById('emailcontent');
 const msg_email="Email is not available";
 
-//Déclarations password 1
+//Déclarations password 
 const password=document.getElementById('psswrd');
 const passwordcontent=document.getElementById('psswrdcontent');
 const msg_pwd="Password must be at least 6 characters";
@@ -23,21 +24,32 @@ const msg_pwd2="Password2 is required";
 
 
 function checkusername(){
-    let node_msg= document.createElement('p');
+    let node_msg = username.querySelector('p.error');
 
-    if (usercontent.value.length>3){
+    if (usercontent.value.length>3 && usercontent.value.length<15){
         usercontent.classList.remove('error');
         usercontent.classList.add('success');
-        if (node_msg.textContent.length>0) {
+        if (node_msg){
             node_msg.remove();
-          }
+        }
         return true;
+    }
+    else if(usercontent.value.length>=15){
+        usercontent.classList.remove('success');
+        usercontent.classList.add('error');
+        let node_msg= document.createElement('p');
+        node_msg.classList.add('error');
+        node_msg.textContent=msg_user_long;
+        username.appendChild(node_msg);
+        return false;
+
     }
     else {
         usercontent.classList.remove('success');
         usercontent.classList.add('error');
+        let node_msg= document.createElement('p');
         node_msg.classList.add('error');
-        node_msg.textContent=msg_user;
+        node_msg.textContent=msg_user_low;
         username.appendChild(node_msg);
         return false;
     }
@@ -47,10 +59,14 @@ function checkusername(){
 
 
 function checkemail(){
+    let node_msg = email.querySelector('p.error');
     
     if(emailcontent.value.includes('@') && emailcontent.value.includes('.')){
         emailcontent.classList.remove('error');
         emailcontent.classList.add('success');
+        if (node_msg){
+            node_msg.remove();
+        }
         return true;
     }
     else {
@@ -66,10 +82,14 @@ function checkemail(){
 
 
 function checkpwd(){
+    let node_msg = password.querySelector('p.error');
 
     if(passwordcontent.value.length>=6){
         passwordcontent.classList.remove('error');
         passwordcontent.classList.add('success');
+        if (node_msg){
+            node_msg.remove();
+        }
         return true;
 
     }
@@ -87,10 +107,14 @@ function checkpwd(){
 
 
 function ckeckpwd2(){
+    let node_msg = password2.querySelector('p.error');
 
     if(password2content.value===passwordcontent.value && password2content.value.length>=6){
         password2content.classList.remove('error');
         password2content.classList.add('success');
+        if (node_msg){
+            node_msg.remove();
+        }
         return true;
     }
     else {
@@ -113,13 +137,15 @@ function handlesubmit(event){
         if(checkusername() && checkemail() &&  checkpwd() && ckeckpwd2()){
 
             console.log(
-                'Username: ' + usercontent.value + " " +
-                'E-mail: ' + emailcontent.value + " " +
-                'Password: ' + passwordcontent.value + " " +
-                'Password2: ' + password2content.value + " " 
+                'Username: ' + usercontent.value + "\n" +
+                'E-mail: ' + emailcontent.value + "\n" +
+                'Password: ' + passwordcontent.value + "\n" +
+                'Password2: ' + password2content.value + "\n" 
             );
         }
         else{
-            console.log('Error in input');
+            console.log(
+                'Error in input'
+            );
         }
     }
